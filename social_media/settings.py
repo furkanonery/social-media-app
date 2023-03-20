d4281fde0077cbb9d523813652c5e7761867dcd3
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-03-20 08:17:56 +0300

    requisite configurations for the project

--- social_media/settings.py
+++ social_media/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    '_profiles.apps.ProfilesConfig',
+    'rest_framework',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
