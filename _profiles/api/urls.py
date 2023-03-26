96fc8ee07095f07e7ddeb6a467090f1e6fb8d840
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-03-26 13:55:17 +0300

    ReadOnlyModelViewSet

--- _profiles/api/urls.py
+++ _profiles/api/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from _profiles.api import views
+from rest_framework.routers import DefaultRouter
+
+# profile_list = views.ProfileViewSet.as_view({'get':'list'})
+# profile_detail = views.ProfileViewSet.as_view({'get':'retrieve'})
+
+router = DefaultRouter()
+router.register(r'profiles',views.ProfileViewSet)
 
 urlpatterns = [
-    path('user-profiles/', views.ProfileList.as_view(), name='profiles'),
+    # path('user-profiles/', views.ProfileList.as_view(), name='profiles'),
+
+    # path('user-profiles/', profile_list, name='profiles'),
+    # path('user-profiles/<int:pk>', profile_detail, name='profile-detail'),
+
+    path('',include(router.urls)),
 ]
--- _profiles/api/views.py
+++ _profiles/api/views.py
@@ -1,9 +1,15 @@
-from rest_framework import generics
+# from rest_framework import generics
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from _profiles.models import Profile
 from _profiles.api.serializers import ProfileSerializer
 
-class ProfileList(generics.ListAPIView):
+# class ProfileList(generics.ListCreateAPIView):
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
+#     permission_classes = [IsAuthenticated]
+
+class ProfileViewSet(ReadOnlyModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
