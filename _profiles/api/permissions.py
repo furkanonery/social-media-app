9266222704b9fbedfd9ad17020dacc4c230400cb
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-03-26 16:57:08 +0300

    GenericViewSet and Mixins created

---
+++ _profiles/api/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+permissions.IsAuthenticatedOrReadOnly
+class ownProfileOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user ==
+            obj.user
+            )
--- _profiles/api/views.py
+++ _profiles/api/views.py
@@ -1,15 +1,23 @@
 # from rest_framework import generics
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet
 from rest_framework.permissions import IsAuthenticated
 from _profiles.models import Profile
 from _profiles.api.serializers import ProfileSerializer
+from rest_framework import mixins
+from _profiles.api.permissions import ownProfileOrReadOnly
 
 # class ProfileList(generics.ListCreateAPIView):
 #     queryset = Profile.objects.all()
 #     serializer_class = ProfileSerializer
 #     permission_classes = [IsAuthenticated]
 
-class ProfileViewSet(ReadOnlyModelViewSet):
+class ProfileViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    # mixins.DestroyModelMixin, silme işlemi istemiyoruz şu an için
+    GenericViewSet
+):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,ownProfileOrReadOnly]
